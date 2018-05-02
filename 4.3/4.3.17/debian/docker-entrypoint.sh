b9e3ab5fb6e64d5b291316bd0861eea937981b6d
Alin Voinea <alin.voinea@eaudeweb.ro>
2018-05-02 11:01:14 +0300

    Refs #91 - Release 4.3.17

--- 4.3/4.3.17/debian/docker-entrypoint.sh
+++ 4.3/4.3.17/debian/docker-entrypoint.sh
@@ -5,17 +5,17 @@ COMMANDS="debug help logtail show stop adduser fg kill quit run wait console for
 START="start restart zeoserver"
 CMD="bin/instance"
 
-python /docker-initialize.py
+gosu plone python /docker-initialize.py
 
 if [ -e "custom.cfg" ]; then
   if [ ! -e "bin/develop" ]; then
-    bin/buildout -c custom.cfg
-    python /docker-initialize.py
+    gosu plone bin/buildout -c custom.cfg
+    gosu plone python /docker-initialize.py
   fi
 fi
 
 if [[ "$1" == "zeo"* ]]; then
-  CMD="bin/zeoserver"
+  CMD="bin/$1"
 fi
 
 if [ -z "$HEALTH_CHECK_TIMEOUT" ]; then
@@ -28,13 +28,13 @@ fi
 
 if [[ $START == *"$1"* ]]; then
   _stop() {
-    $CMD stop
+    gosu plone $CMD stop
     kill -TERM $child 2>/dev/null
   }
 
   trap _stop SIGTERM SIGINT
-  $CMD start
-  $CMD logtail &
+  gosu plone $CMD start
+  gosu plone $CMD logtail &
   child=$!
 
   pid=`$CMD status | sed 's/[^0-9]*//g'`
@@ -50,7 +50,7 @@ if [[ $START == *"$1"* ]]; then
   fi
 else
   if [[ $COMMANDS == *"$1"* ]]; then
-    exec bin/instance "$@"
+    exec gosu plone bin/instance "$@"
   fi
   exec "$@"
 fi
