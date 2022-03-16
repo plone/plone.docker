3cce727afab9b7c98fe0567d3ae69e5b08893631
alessandro.ceglie <alessandro.ceglie@uniba.it>
2022-03-16 10:41:50 +0100

    fix #165 shared-blob-dir

--- 5.2/5.2.7/debian/docker-initialize.py
+++ 5.2/5.2.7/debian/docker-initialize.py
@@ -263,8 +263,11 @@ class Environment(object):
         # If we need to create a plonesite and we have a zeo setup
         # configure collective.recipe.plonesite properly
         server = self.env.get("ZEO_ADDRESS", None)
+        shared_blob_dir=self.env.get("ZEO_SHARED_BLOB_DIR", "off")
+
         if server:
-            buildout += ZEO_INSTANCE_TEMPLATE.format(zeoaddress=server)
+            buildout += ZEO_INSTANCE_TEMPLATE.format(zeoaddress=server,
+                                                     shared_blob_dir=shared_blob_dir)
 
         # Add RelStorage configuration if needed
         if relstorage:
@@ -343,7 +346,8 @@ ZEO_INSTANCE_TEMPLATE = """
 [instance]
 zeo-client = true
 zeo-address = {zeoaddress}
-shared-blob = off
+shared-blob = {shared_blob_dir}
+
 http-fast-listen = off
 """
 
